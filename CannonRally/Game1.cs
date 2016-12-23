5eacf18150b67bcf4fb09f9bf274eadbd2ff2107
Janusz Sołtysiak <jsoltysiak@gmail.com>
2016-12-23 12:21:37 +0100

    Add Tire class with some basic physics

--- CannonRally/Game1.cs
+++ CannonRally/Game1.cs
@@ -1,16 +1,25 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using FarseerPhysics;
+using FarseerPhysics.DebugView;
+using FarseerPhysics.Dynamics;
+using FarseerPhysics.Factories;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 namespace CannonRally
 {
     /// <summary>
-    /// This is the main type for your game.
+    ///     This is the main type for your game.
     /// </summary>
     public class Game1 : Game
     {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        private SpriteFont _font;
+        private Tire _tire;
+        private DebugViewXNA DebugView;
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+        private World world;
 
         public Game1()
         {
@@ -19,10 +28,10 @@ namespace CannonRally
         }
 
         /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
+        ///     Allows the game to perform any initialization it needs to before starting to run.
+        ///     This is where it can query for any required services and load any non-graphic
+        ///     related content.  Calling base.Initialize will enumerate through any components
+        ///     and initialize them as well.
         /// </summary>
         protected override void Initialize()
         {
@@ -32,20 +41,33 @@ namespace CannonRally
         }
 
         /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
+        ///     LoadContent will be called once per game and is the place to load
+        ///     all of your content.
         /// </summary>
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            world = new World(Vector2.Zero);
+
+            var tireSprite = new Sprite(Content.Load<Texture2D>("tire"));
+            _tire = new Tire(BodyFactory.CreateRectangle(world, 5f, 5f, 1f, new Vector2(1, 2)), tireSprite);
+
+            if (DebugView == null)
+            {
+                DebugView = new DebugViewXNA(world);
+                DebugView.RemoveFlags(DebugViewFlags.Shape);
+                DebugView.RemoveFlags(DebugViewFlags.Joint);
+                DebugView.DefaultShapeColor = Color.White;
+                DebugView.SleepingShapeColor = Color.LightGray;
+                DebugView.LoadContent(GraphicsDevice, Content);
+            }
         }
 
         /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
+        ///     UnloadContent will be called once per game and is the place to unload
+        ///     game-specific content.
         /// </summary>
         protected override void UnloadContent()
         {
@@ -53,29 +75,53 @@ namespace CannonRally
         }
 
         /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
+        ///     Allows the game to run logic such as updating the world,
+        ///     checking for collisions, gathering input, and playing audio.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if ((GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed) ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            world.Step(Math.Min((float) gameTime.ElapsedGameTime.TotalSeconds, 1f/30f));
+            _tire.Update(gameTime);
 
             base.Update(gameTime);
         }
 
         /// <summary>
-        /// This is called when the game should draw itself.
+        ///     This is called when the game should draw itself.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            var SimProjection = Matrix.CreateOrthographicOffCenter(
+                0f,
+                ConvertUnits.ToSimUnits(GraphicsDevice.Viewport.Width),
+                ConvertUnits.ToSimUnits(GraphicsDevice.Viewport.Height), 0f, 0f, 
+                1f);
+            var SimView = Matrix.Identity;
+            var View = Matrix.Identity;
+            var projection = Matrix.CreateOrthographicOffCenter(
+                0f,
+                ConvertUnits.ToSimUnits(graphics.GraphicsDevice.Viewport.Width),
+                ConvertUnits.ToSimUnits(graphics.GraphicsDevice.Viewport.Height), 0f, 0f,
+                1f);
+
+            spriteBatch.Begin(0, null, null, null, null, null, View);
+            //spriteBatch.Begin();
+            _tire.Draw(spriteBatch);
+
+
+            DebugView.RenderDebugData(ref projection);
+
+            spriteBatch.End();
+            //DebugView.RenderDebugData(SimProjection, SimView);
+
 
             base.Draw(gameTime);
         }
---
+++ CannonRally/Tire.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using FarseerPhysics;
+using FarseerPhysics.Dynamics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace CannonRally
+{
+    public class Tire
+    {
+        public Tire(Body body, Sprite sprite)
+        {
+            Body = body;
+            Sprite = sprite;
+
+            Body.BodyType = BodyType.Dynamic;
+            Body.ApplyLinearImpulse(new Vector2(20.0f, 20.0f));
+        }
+
+        public Vector2 Position { get; set; }
+        public Body Body { get; set; }
+        public Sprite Sprite { get; set; }
+
+        public float DragForceMultiplier { get; set; } = 4f;
+
+        public void Update(GameTime gameTime)
+        {
+            HandleInput();
+            UpdateFriction();
+        }
+
+        private void HandleInput()
+        {
+            const float maxForwardSpeed = 100f;
+            const float maxBackwardSpeed = -20f;
+            const float maxDriveForce = -20f;
+
+            float desiredSpeed = 0.0f;
+            var keyboardState = Keyboard.GetState();
+            if (keyboardState.IsKeyDown(Keys.W))
+            {
+                desiredSpeed = maxForwardSpeed;
+            }
+            if (keyboardState.IsKeyDown(Keys.S))
+            {
+                desiredSpeed = maxBackwardSpeed;
+            }
+
+            var currentForwardNormal = Body.GetWorldVector(new Vector2(0, 1));
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Sprite.Texture, ConvertUnits.ToDisplayUnits(Body.Position), null, Color.White,
+                Body.Rotation, Sprite.Origin, 1f, SpriteEffects.None, 0f);
+        }
+
+        private void UpdateFriction()
+        {
+            var impulse = Body.Mass*-GetLateralVelocity();
+            Body.ApplyLinearImpulse(impulse, Body.WorldCenter);
+            Body.ApplyAngularImpulse(0.1f * Body.Inertia * -Body.AngularVelocity);
+
+            Vector2 currentForwardNormal = GetForwardVelocity();
+            if (Math.Abs(currentForwardNormal.Length()) > float.Epsilon) currentForwardNormal.Normalize();
+            Vector2 dragForceMagniture = currentForwardNormal*(-DragForceMultiplier);
+            Body.ApplyForce(dragForceMagniture, Body.WorldCenter);
+        }
+
+        private Vector2 GetLateralVelocity()
+        {
+            var currentRightNormal = Body.GetWorldVector(new Vector2(1, 0));
+            return Vector2.Dot(currentRightNormal, Body.LinearVelocity)*currentRightNormal;
+        }
+
+        private Vector2 GetForwardVelocity()
+        {
+            var currentForwardNormal = Body.GetWorldVector(new Vector2(0, 1));
+            return Vector2.Dot(currentForwardNormal, Body.LinearVelocity)*currentForwardNormal;
+        }
+    }
+}
